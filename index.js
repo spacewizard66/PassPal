8dea1076cfc0a506f41d8d1b32c1b9facd90ef98
spacewizard66 <mason4work@outlook.com>
2024-03-11 02:36:18 -0400

    Added new copy clipboard button. Cleaned and refactored existing js code.

--- index.js
+++ index.js
@@ -106,43 +106,42 @@ function passwordGenerate(len, upper, lower, number, symbol) {
 
 
 const body = document.body;
-const nav = document.body.querySelector(".nav");
-const main = document.body.querySelector(".main");
-const settings = document.body.querySelector(".settings");
-const result = document.body.querySelector(".result");
+const nav = document.body.querySelector('.nav');
+const main = document.body.querySelector('.main');
+const settings = document.body.querySelector('.settings');
+const result = document.body.querySelector('.result');
 
 // Dark Mode/Light Mode Toggle Button
-darkModeButton = document.querySelector(".nav__toggle");
+darkModeButton = document.querySelector('.nav__toggle');
 darkModeButton.addEventListener('click', () => {
-	nav.classList.toggle("nav--active");
-	body.classList.toggle("body--active");
-	main.classList.toggle("main--active");
-	slider.classList.toggle("slider--active");
-	settings.classList.toggle("settings--active");
-	result.classList.toggle("result--active");
+	nav.classList.toggle('nav--active');
+	body.classList.toggle('body--active');
+	main.classList.toggle('main--active');
+	slider.classList.toggle('slider--active');
+	settings.classList.toggle('settings--active');
+	result.classList.toggle('result--active');
 });
 
 
-const slider = document.querySelector(".slider");
-const sliderValue = document.querySelector(".slider__title");
-
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.slider__title');
 // Sets default slider value //
-sliderValue.setAttribute("data-length", 35);
-// Using Event Listener to the slider change value accordingly //
-slider.querySelector("input").addEventListener("input", (event) => {
-	sliderValue.setAttribute("data-length", event.target.value);
+sliderValue.setAttribute('data-length', 35);
+// changes slider value accordingly //
+slider.querySelector('input').addEventListener('input', (event) => {
+	sliderValue.setAttribute('data-length', event.target.value);
 });
 
 
 // Checks which options are selected and plugs in to Password Generator //
-const generateButton = document.getElementById("generate");
-generateButton.addEventListener("click", () => {
-	const uppercase = document.getElementById("uppercase").checked;
-	const lowercase = document.getElementById("lowercase").checked;
-	const number = document.getElementById("number").checked;
-	const symbol = document.getElementById("symbol").checked;
+const generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', () => {
+	const uppercase = document.getElementById('uppercase').checked;
+	const lowercase = document.getElementById('lowercase').checked;
+	const number = document.getElementById('number').checked;
+	const symbol = document.getElementById('symbol').checked;
 	passwordGenerate(
-		sliderValue.getAttribute("data-length"),
+		sliderValue.getAttribute('data-length'),
 		uppercase,
 		lowercase,
 		number,
@@ -152,43 +151,41 @@ generateButton.addEventListener("click", () => {
 
 
 function showPassword(password) {
-	document.getElementById("result").innerText = password;
-	if (!document.getElementById("copy-btn")) {
+	document.getElementById('result').innerText = password;
+	/* if (!document.getElementById("copy-btn")) {
 		document.querySelector(".result").innerHTML +=
 		'<button id="copy-btn" onclick="copyPassword()"><img src="icon/copy.svg" width="16px"></button>';
-	}
+	} */
 	// document.querySelector('.result').innerHTML += '<button id="copy-btn" onclick="copyPassword()"><img src="copy.svg" width="16px"></button>'; //
 }
 
-// Error if no option is selected //
+
+// Error if no setting is selected //
 function showError() {
-	document.getElementById("result").innerText = "Please select at least one attribute";
-	if (document.getElementById("copy-btn")) {
-		document.getElementById("copy-btn").remove();
-	}
+	document.getElementById('result').innerText = "Please select at least one attribute";
 }
 
-const copyInfo = document.querySelector(".result__info.right");
-const copiedInfo = document.querySelector(".result__info.left");
-const copyBtn = document.querySelector("#copy-btn");
+
 // Copy password to clipboard //
-function copyPassword() {
-	const textarea = document.createElement("textarea");
-	const password = document.getElementById("result").innerText;
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand("copy");
-	textarea.remove();
-}
+document.body.querySelector('.copy-test').addEventListener('click', () => {
+		if (!navigator.clipboard) {
+			// Clipboard API not available
+			return
+		}
+		const password = result.innerText;
+		try {
+			navigator.clipboard.writeText(password);
+		} catch (err) {
+			console.error('Failed to copy!', err);
+		}
+	}
+)
 
-// Getting current year for footer //
-const d = new Date();
-const currYear = d.getFullYear();
-document.querySelector(
-	".footer__copyright"
-).innerText = `© ${currYear}, Made with ❤️ by Mason Galat.`;
 
+// Fetches current year for footer //
+const date = new Date();
+const currentYear = date.getFullYear();
+document.querySelector('.footer__copyright').innerText = `© ${currentYear}, Made with ❤️ by Mason Galat.`;
 
 
 // Disable right click //
